b89e0285118806b5d67d107b5f7ea9ddc339c3d8
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-03-07 17:05:19 -0300

    feat: add ui project

---
+++ packages/ui/src/components/Guide/interface.ts
@@ -0,0 +1,23 @@
+import type { JSX } from "react";
+
+export interface StepData {
+    name: string;
+    content: JSX.Element;
+    orientation: {
+        vertical: 'top' | 'bottom' | 'center';
+        horizontal: 'left' | 'right' | 'center';
+    };
+    callback?: {
+        open?: () => void;
+        start?: () => void;
+        finish?: () => void;
+    };
+}
+
+export interface StepLocalData extends StepData {
+    visible: boolean;
+}
+
+export type Options = {
+    finishCallback?: (index: number) => void;
+}
