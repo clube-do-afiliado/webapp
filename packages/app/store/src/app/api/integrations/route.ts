ab76c35112170c6e7d741b775f3a72d3451daca2
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-19 10:08:51 -0300

    feat: add runtime edge to app

--- packages/app/store/src/app/api/integrations/route.ts
+++ packages/app/store/src/app/api/integrations/route.ts
@@ -1,5 +1,7 @@
 import { integrationsServices } from '@/services/core';
 
+export const runtime = 'edge';
+
 export async function GET() {
     const integrations = await integrationsServices.list();
 
